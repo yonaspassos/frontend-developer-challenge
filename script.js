9c18ee192d73ea13e634edbfb7476f904251d3f4
Yoná <yona.nantes@gmail.com>
2019-07-20 19:24:59 -0300

    refactor: Change API request call to be asynchronous

--- script.js
+++ script.js
@@ -5,22 +5,31 @@ window.onload = function() {
 };
 
 function getProducts(url) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", `https:${url}`, false );
-    xmlHttp.send( null );
-    var data = JSON.parse(xmlHttp.responseText);
-    nextPage = data.nextPage;
-    var template = document.getElementById("product-card");
-    var productsWrapper = document.getElementById("products-wrapper");
-    data.products.forEach(function(product) {
-        let element = document.importNode(template.content, true);
-        element.querySelector("img").src = "https:" + product.image;
-        element.querySelector("h3").innerHTML = product.name;
-        element.querySelector(".description").innerHTML = product.description;
-        element.querySelector(".old-price").innerHTML = `De: R$${product.oldPrice.toFixed(2)}`;
-        element.querySelector(".current-price").innerHTML = `Por: R$${product.price.toFixed(2)}`;
-        element.querySelector(".installments").innerHTML = `ou ${product.installments.count}x de R$${product.installments.value.toFixed(2)}`;
-        productsWrapper.appendChild(element)
-        console.log(product);
+    request(url, function(responseText) {
+        var data = JSON.parse(responseText);
+        nextPage = `//${data.nextPage}`;
+        var template = document.getElementById("product-card");
+        var productsWrapper = document.getElementById("products-wrapper");
+        data.products.forEach(function(product) {
+            var element = document.importNode(template.content, true);
+            element.querySelector("img").src = product.image;
+            element.querySelector("h3").innerHTML = product.name;
+            element.querySelector(".description").innerHTML = product.description;
+            element.querySelector(".old-price").innerHTML = `De: R$${product.oldPrice.toFixed(2)}`;
+            element.querySelector(".current-price").innerHTML = `Por: R$${product.price.toFixed(2)}`;
+            element.querySelector(".installments").innerHTML = `ou ${product.installments.count}x de R$${product.installments.value.toFixed(2)}`;
+            productsWrapper.appendChild(element);
+        });
     });
 };
+
+function request(url, callback) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText)
+        }
+    }
+    xmlHttp.open("GET", url, true);
+    xmlHttp.send();  
+}
